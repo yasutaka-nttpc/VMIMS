111542c84e2338abbbffc6b135b58a8f9c222842
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-12-26 00:33:15 +0900

    informationテーブル作成

--- db/migrate/20171225140533_create_information.rb
+++ db/migrate/20171225140533_create_information.rb
@@ -1,8 +1,40 @@
 class CreateInformation < ActiveRecord::Migration[5.1]
+  UPDATE_TIMESTAMP = 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+
   def change
     create_table :information do |t|
-
-      t.timestamps
+      # idカラムはデフォルトで作られる
+      # t.integer :id, :limit => 11, :null => false, comment: 'ID'
+      t.datetime :created_at, comment: '登録日時'
+      t.column :updated_at, UPDATE_TIMESTAMP, null: true, comment: '更新日時'
+      t.integer :group, :limit => 4, comment: 'グループID'
+      t.integer :service, :limit => 4, comment: 'サービスID'
+      t.integer :category, :limit => 4, comment: '機器種別ID'
+      t.integer :environment, :limit => 4, comment: '環境ID'
+      t.string :name, :limit => 255, :null => false, comment: '機器名'
+      t.string :serial, :limit => 255, comment: 'シリアルナンバー'
+      t.string :server_name, :limit => 255, :null => false, comment: 'サーバー名'
+      t.string :support_contact, :limit => 255, comment: '保守ベンダー'
+      t.date :support_contact_period, comment: '保守契約期間'
+      t.string :support_id, :limit => 255, default: :serial, comment: '保守番号'
+      t.string :hard_support_conditions, :limit => 255, comment: 'ハード保守条件'
+      t.string :hard_contact_tel, :limit => 255, comment: 'ハード保守電話番号'
+      t.string :hard_contact_mail, :limit => 255, comment: 'ハード保守メールアドレス'
+      t.string :hard_contact_other, :limit => 1024, comment: 'ハード保守その他'
+      t.string :soft_support_conditions, :limit => 255, comment: 'ソフト保守条件'
+      t.string :soft_contact_tel, :limit => 255, comment: 'ソフト保守電話番号'
+      t.string :soft_contact_mail, :limit => 255, comment: 'ソフト保守メールアドレス'
+      t.string :soft_contact_other, :limit => 1024, comment: 'ソフト保守その他'
+      t.string :os_version, :limit => 255, comment: 'OSバージョン'
+      t.date :eos_hard, comment: 'ハード保守期限'
+      t.date :eos_soft, comment: 'ソフト保守期限'
+      t.string :sales_contact, :limit => 255, comment: '営業担当者'
+      t.string :sales_contact_tel, :limit => 255, comment: '営業電話番号'
+      t.string :sales_contact_mail, :limit => 255, comment: '営業メールアドレス'
+      t.string :remarks_1, :limit => 1024, comment: '備考1'
+      t.string :remarks_2, :limit => 1024, comment: '備考2'
+      t.string :remarks_3, :limit => 1024, comment: '備考3'
+      t.integer :non_alert_flag, :limit => 3, comment: '保守期限アラート無効'
     end
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171225140533) do
+
+  create_table "information", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", comment: "登録日時"
+    t.timestamp "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false, comment: "更新日時"
+    t.integer "group", comment: "グループID"
+    t.integer "service", comment: "サービスID"
+    t.integer "category", comment: "機器種別ID"
+    t.integer "environment", comment: "環境ID"
+    t.string "name", null: false, comment: "機器名"
+    t.string "serial", comment: "シリアルナンバー"
+    t.string "server_name", null: false, comment: "サーバー名"
+    t.string "support_contact", comment: "保守ベンダー"
+    t.date "support_contact_period", comment: "保守契約期間"
+    t.string "support_id", default: "serial", comment: "保守番号"
+    t.string "hard_support_conditions", comment: "ハード保守条件"
+    t.string "hard_contact_tel", comment: "ハード保守電話番号"
+    t.string "hard_contact_mail", comment: "ハード保守メールアドレス"
+    t.string "hard_contact_other", limit: 1024, comment: "ハード保守その他"
+    t.string "soft_support_conditions", comment: "ソフト保守条件"
+    t.string "soft_contact_tel", comment: "ソフト保守電話番号"
+    t.string "soft_contact_mail", comment: "ソフト保守メールアドレス"
+    t.string "soft_contact_other", limit: 1024, comment: "ソフト保守その他"
+    t.string "os_version", comment: "OSバージョン"
+    t.date "eos_hard", comment: "ハード保守期限"
+    t.date "eos_soft", comment: "ソフト保守期限"
+    t.string "sales_contact", comment: "営業担当者"
+    t.string "sales_contact_tel", comment: "営業電話番号"
+    t.string "sales_contact_mail", comment: "営業メールアドレス"
+    t.string "remarks_1", limit: 1024, comment: "備考1"
+    t.string "remarks_2", limit: 1024, comment: "備考2"
+    t.string "remarks_3", limit: 1024, comment: "備考3"
+    t.integer "non_alert_flag", limit: 3, comment: "保守期限アラート無効"
+  end
+
+end
