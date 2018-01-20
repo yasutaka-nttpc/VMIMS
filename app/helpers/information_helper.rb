0d25b179505c9b705fb0d2a889fc9b9c49d48b61
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2018-01-21 01:02:53 +0900

    編集機能

--- app/helpers/information_helper.rb
+++ app/helpers/information_helper.rb
@@ -1,2 +1,9 @@
 module InformationHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_information_path
+    elsif action_name == 'edit'
+      information_path
+    end
+  end
 end
