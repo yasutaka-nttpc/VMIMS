2f910eb34632f9915839a37282659eb5db55cd51
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2018-01-24 22:51:57 +0900

    topページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'information#index'
+
+  root 'top#index'
 
   resources :information, only: [:index, :new, :create, :destroy, :edit, :update] do
     post :confirm, on: :collection
   end
-  #get 'information' => 'information#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
