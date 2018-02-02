46462dabf775448d960903b5ca57a9adddb3fff7
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2018-02-02 17:17:46 +0900

    電話番号とメールアドレスのバリデーション

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,7 +1,11 @@
 class Information < ApplicationRecord
+  VALID_PHONE_REGEX = /\A(\d|-){10,11}\z/
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
 
   validates :name, :server_name, presence:true
+  validates :hard_contact_tel, format: { with: VALID_PHONE_REGEX }, if: -> { not hard_contact_tel.blank? }
+  validates :soft_contact_tel, format: { with: VALID_PHONE_REGEX }, if: -> { not soft_contact_tel.blank? }
+  validates :sales_contact_tel, format: { with: VALID_PHONE_REGEX }, if: -> { not sales_contact_tel.blank? }
   validates :hard_contact_mail, format: { with: VALID_EMAIL_REGEX }, if: -> { not hard_contact_mail.blank? }
   validates :soft_contact_mail, format: { with: VALID_EMAIL_REGEX }, if: -> { not soft_contact_mail.blank? }
   validates :sales_contact_mail, format: { with: VALID_EMAIL_REGEX }, if: -> { not sales_contact_mail.blank? }
